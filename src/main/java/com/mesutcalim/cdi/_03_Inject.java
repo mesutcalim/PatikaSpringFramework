b3540097d0705444bc719e7bf0904d56d567ac1e
mesutcalim <mesutcalim.eee@gmail.com>
2022-10-13 01:16:01 +0300

    @Inject Yapilari (Tuketen)

---
+++ src/main/java/com/mesutcalim/cdi/_03_Inject.java
@@ -0,0 +1,18 @@
+package com.mesutcalim.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.util.List;
+
+@Named(value = "injecttuto")
+@ApplicationScoped
+public class _03_Inject {
+    @Inject
+    List<String> tuketen;
+
+    public List<String> getTuketen() {
+        return tuketen;
+    }
+}
